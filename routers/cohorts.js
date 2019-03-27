52d1ba09886bad832a2ecc060bcccc0aa78c6575
clem9281 <clem9281@gmail.com>
2019-03-27 14:36:24 -0700

    added a post new cohort endpoint

--- routers/cohorts.js
+++ routers/cohorts.js
@@ -4,13 +4,29 @@ const knexConfig = require("../knexfile");
 const router = express.Router();
 const db = knex(knexConfig.development);
 
-router.route("/").get(async (req, res) => {
-  try {
-    const cohorts = await db("cohorts");
-    res.status(200).json(cohorts);
-  } catch (error) {
-    res.status(500).json({ error: "Whoops, something went wrong" });
-  }
-});
+router
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      const cohorts = await db("cohorts");
+      res.status(200).json(cohorts);
+    } catch (error) {
+      res.status(500).json({ error: "Whoops, something went wrong" });
+    }
+  })
+  .post(async (req, res) => {
+    if (!req.body.name) {
+      return res.status(400).json({ error: "The cohort name is required" });
+    }
+    try {
+      const newCohortId = await db("cohorts").insert(req.body);
+      const newCohort = await db("cohorts")
+        .where({ id: newCohortId[0] })
+        .first();
+      res.status(201).json(newCohort);
+    } catch (error) {
+      res.status(500).json({ error: "Sorry, we couldn't add that cohort" });
+    }
+  });
 
 module.exports = router;
