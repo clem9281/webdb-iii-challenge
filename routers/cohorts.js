04e013d765d80432725d8bbdc189df5269b93633
clem9281 <clem9281@gmail.com>
2019-03-27 14:45:35 -0700

    added a delete cohort endpoint

--- routers/cohorts.js
+++ routers/cohorts.js
@@ -29,22 +29,42 @@ router
     }
   });
 
-router.route("/:id").get(async (req, res) => {
-  try {
-    const cohort = await db("cohorts")
-      .where({ id: req.params.id })
-      .first();
-    if (!cohort) {
-      return res
-        .status(404)
-        .json({ error: "Sorry, we couldn't find a cohort at that id" });
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    try {
+      const cohort = await db("cohorts")
+        .where({ id: req.params.id })
+        .first();
+      if (!cohort) {
+        return res
+          .status(404)
+          .json({ error: "Sorry, we couldn't find a cohort at that id" });
+      }
+      res.status(200).json(cohort);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Sorry, we couldn't find that cohort at this time" });
     }
-    res.status(200).json(cohort);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Sorry, we couldn't find that cohort at this time" });
-  }
-});
+  })
+  .delete(async (req, res) => {
+    try {
+      const deleted = await db("cohorts")
+        .where({ id: req.params.id })
+        .del();
+      if (deleted === 0) {
+        return res
+          .status(404)
+          .json({ error: "There is no cohort entry at that id" });
+      }
+      const cohorts = await db("cohorts");
+      res.status(200).json(cohorts);
+    } catch (error) {
+      res.status(500).json({
+        error: "Sorry, we could not delete that cohort entry at this time"
+      });
+    }
+  });
 
 module.exports = router;
