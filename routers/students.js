1771c05b5789ae6cb07026257e301b20adf90043
clem9281 <clem9281@gmail.com>
2019-03-27 15:23:43 -0700

    added an endpoint to create a new student

--- routers/students.js
+++ routers/students.js
@@ -4,15 +4,47 @@ const knexConfig = require("../knexfile");
 const router = express.Router();
 const db = knex(knexConfig.development);
 
-router.route("/").get(async (req, res) => {
-  try {
-    const students = await db("students");
-    res.status(200).json(students);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Sorry, we couldn't get the students at this time" });
-  }
-});
+router
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      const students = await db("students");
+      res.status(200).json(students);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Sorry, we couldn't get the students at this time" });
+    }
+  })
+  .post(async (req, res) => {
+    const { name, cohort_id } = req.body;
+    if (!name) {
+      return res.status(400).json({
+        error: "The student name is required"
+      });
+    }
+    try {
+      //  for fun: check and make sure you are only adding a student to an existing cohort
+      if (cohort_id) {
+        const cohort = await db("cohorts")
+          .where({ id: cohort_id })
+          .first();
+        if (!cohort) {
+          return res
+            .status(400)
+            .json({ error: "The supplied cohort id must exist" });
+        }
+      }
+      const newStudentId = await db("students").insert(req.body);
+      const newStudent = await db("students")
+        .where({ id: newStudentId[0] })
+        .first();
+      res.status(201).json(newStudent);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Sorry, we could not add a new student at this time" });
+    }
+  });
 
 module.exports = router;
