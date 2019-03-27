e40d64c17db4dc919448829c11825aa74d344176
clem9281 <clem9281@gmail.com>
2019-03-27 14:25:39 -0700

    added a get cohorts endpoint

--- routers/cohorts.js
+++ routers/cohorts.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const knex = require("knex");
+const knexConfig = require("../knexfile");
 const router = express.Router();
+const db = knex(knexConfig.development);
+
+router.route("/").get(async (req, res) => {
+  try {
+    const cohorts = await db("cohorts");
+    res.status(200).json(cohorts);
+  } catch (error) {
+    res.status(500).json({ error: "Whoops, something went wrong" });
+  }
+});
 
 module.exports = router;
